8d950367d7c86a8f951951939a8b32813c38a1ef
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2023-05-17 13:48:31 -0400

    integracion de configuracion ORM JPA

--- src/main/java/ufro/isoft/desafio1/Desafio1Application.java
+++ src/main/java/ufro/isoft/desafio1/Desafio1Application.java
@@ -1,13 +1,20 @@
 package ufro.isoft.desafio1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ufro.isoft.desafio1.respository.CamionetaRepository;
 
 @SpringBootApplication
 public class Desafio1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio1Application.class, args);
 		System.out.println("***************Hola mundo*****************");
+	}
+	@Bean
+	CommandLineRunner commandLineRunner(CamionetaRepository camionetaRepository, CamionetaRepository sedanRepository, CamionetaRepository suvRepository) {
+		return args -> {
+		};
 	}
 }
