f6dbfe9efb3bc963caf63e8277d409b474539429
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2023-05-18 09:58:21 -0400

    se reintegra el archivo yml de pipeline para comprobar correcto funcionamiento

--- src/main/java/ufro/isoft/desafio1/Controller/AutomovilesController.java
+++ src/main/java/ufro/isoft/desafio1/Controller/AutomovilesController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutomovilesController {
     @GetMapping("automoviles")
     public String vistaAutomoviles() {
-        return "vistaAutom  oviles";
+        return "/vistaAutomoviles";
     }
 }
