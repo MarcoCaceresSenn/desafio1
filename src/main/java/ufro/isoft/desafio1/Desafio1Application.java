7ee90fc9db92038892bb4ecbe36392d4bcf3f57d
Carlo Negroni <c.negroni01@ufromail.cl>
2023-05-20 18:35:29 -0400

    Correción de metedos

--- src/main/java/ufro/isoft/desafio1/Desafio1Application.java
+++ src/main/java/ufro/isoft/desafio1/Desafio1Application.java
@@ -5,17 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import ufro.isoft.desafio1.model.Sedan;
+import ufro.isoft.desafio1.respository.CamionetaRepository;
+import ufro.isoft.desafio1.respository.SedanRepository;
+import ufro.isoft.desafio1.respository.SuvRepository;
+
 @SpringBootApplication
 public class Desafio1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio1Application.class, args);
 		System.out.println("***************Hola mundo*****************");
 	}
-	/*
+	
 	@Bean
-	CommandLineRunner commandLineRunner(CamionetaRepository camionetaRepository, CamionetaRepository sedanRepository, CamionetaRepository suvRepository) {
+	CommandLineRunner commandLineRunner(CamionetaRepository camionetaRepository, SedanRepository sedanRepository, SuvRepository suvRepository) {
 		return args -> {
 		};
 	}
-	*/
+	
 }
